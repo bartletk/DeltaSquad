7fb3df42f4b37dc277ddf7434f920a6da3a01df5
babersi <35636185+babersi@users.noreply.github.com>
2018-02-24 22:21:03 -0600

    Add files via upload

---
+++ htdocs/Student Dropdown.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+</head>
+<body>
+  <fieldset>
+     <legend>Student View</legend>
+         <p>
+            <label>Choose Your Semester: </label><br/>
+            <select name="semester">
+               <option>Semester 1</option>
+               <option>Semester 2</option>
+               <option>Semester 3</option>
+               <option>Semester 4</option>
+               <option>Semester 5</option>
+            </select>
+         </p>
+         <p>
+            <label>Choose Course: </label><br/>
+            <select size="1" name="course">
+               <option value="NURS 2000">2000</option>
+            </select>
+         </p>
+         <p>
+            <label>CRN: </label><br/>
+           <select name="crn">
+                <optgroup label="Semester 1">
+                  <option>362111</option>
+            </select>
+         </p>          
+         <p>    
+
+  </fieldset>
+</form>
+</body>
+</html>
