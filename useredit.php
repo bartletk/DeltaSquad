f460308327aa0ef72cc412211714a9533533049c
Aakashpoudel <poudelaakash2000@gmail.com>
2018-04-18 18:56:10 -0500

    Front end templates and connection to the backend

--- useredit.php
+++ useredit.php
@@ -13,6 +13,45 @@ include("header.php");
 <body>
 <body>
 
+
+<div class="card card1">
+  <h5><strong>User Account Edit: admin</strong></h5>
+ <div class="row row1">
+  <form class="col s12">
+    <div class="row">
+      <div class="input-field col s12">
+        <input id="fname" name="fname" type="text" class="validate" required="" aria-required="true">
+        <label for="fname">Name</label>
+      </div>
+      <div class="input-field col s12">
+        <input id="email2" type="email" class="validate" required="" aria-required="true">
+        <label for="email2">Email</label>
+      </div>
+      <div class="input-field col s12">
+        <input id="current-password" name="current-password" type="text" class="validate" required="" aria-required="true">
+        <label for="current-password">Current Password</label>
+      </div>
+      <div class="input-field col s12">
+        <input id="new-password" name="new-passowrd" type="text" class="validate" required="" aria-required="true">
+        <label for="example">New Password</label>
+      </div>
+     
+      <div class="input-field col s12">
+        <button class="btn waves-effect waves-light" type="submit" name="action">Submit</button>
+      </div>
+    </div>	
+  				</form>
+			</div>
+		</div>
+	</body>
+</html>
+
+
+<?php
+
+$page = "useredit.php";
+?>
+<div>
 <?php
 /**
  * User has submitted form without errors and user's
@@ -26,66 +65,76 @@ if(isset($_SESSION['useredit'])){
        ."<a href=\"main.php\">Main</a>.</p>";
 }
 else{
-	/**
+?>
+
+<?php
+/**
  * If user is not logged in, then do not display anything.
  * If user is logged in, then display the form to edit
  * account information, with the current email address
  * already in the field.
  */
 if($session->logged_in){
-	?>
-<div class="card card1">
-  <h5><strong>User Account Edit: <?php echo $session->username; ?></strong></h5>
- <div class="row row1">
-  <form class="col s12" action="process.php" method="POST">
-    <div class="row">
-      <div class="input-field col s12">
-        <input id="fname" name="name" type="text" class="validate" required="" aria-required="true" value="<?php
+?>
+
+<h1>User Account Edit : <?php echo $session->username; ?></h1>
+<?php
+if($form->num_errors > 0){
+   echo "<td><font size=\"2\" color=\"#ff0000\">".$form->num_errors." error(s) found</font></td>";
+}
+?>
+<div>
+	<form action="process.php" method="POST">
+		<p>Name: </p>
+		<p>
+			<input type="text" name="name" maxlength="50" value="<?php
 			if($form->value("name") == ""){
 				echo $session->userinfo['name'];
 			}else{
 				echo $form->value("name");
 			}
 			?>">
-        <label for="name">Name</label>
-		<?php echo $form->error("name"); ?>
-      </div>
-      <div class="input-field col s12">
-        <input id="email2" type="email" class="validate" required="" aria-required="true" value="<?php
+			<?php echo $form->error("name"); ?>
+		</p>
+		<div></div>
+		<p>Current Password: </p>
+		<p>
+			<input type="password" name="curpass" maxlength="30" value="<?php echo $form->value("curpass"); ?>">
+			<?php echo $form->error("curpass"); ?>
+		</p>
+		<div></div>
+		<p>New Password: </p>
+		<p>
+			<input type="password" name="newpass" maxlength="30" value="<?php echo $form->value("newpass"); ?>">
+			<?php echo $form->error("newpass"); ?>
+		</p>
+		<div></div>
+		<p>Email: </p>
+		<p>
+			<input type="text" name="email" maxlength="50" value="<?php
 			if($form->value("email") == ""){
 				echo $session->userinfo['email'];
 			}else{
 				echo $form->value("email");
 			}
 			?>">
-        <label for="email">Email</label>
-		<?php echo $form->error("email"); ?>
-      </div>
-      <div class="input-field col s12">
-        <input id="current-password" name="curpass" type="password" class="validate" required="" aria-required="true" value="<?php echo $form->value("curpass"); ?>">
-        <label for="current-password">Current Password</label>
-		<?php echo $form->error("curpass"); ?>
-      </div>
-      <div class="input-field col s12">
-        <input id="new-password" name="newpass" type="password" class="validate" required="" aria-required="true" value="<?php echo $form->value("newpass"); ?>">
-        <label for="new-password">New Password</label>
-		<?php echo $form->error("newpass"); ?>
-      </div>
-     <input type="hidden" name="subedit" value="1" />
-      <div class="input-field col s12">
-        <button class="btn waves-effect waves-light" type="submit" name="action">Submit</button>
-      </div>
-    </div>	
-  				</form>
-			</div>
-		</div>
-		<?php
+			<?php echo $form->error("email"); ?>
+		</p>
+		<div></div>
+		<p>
+			<input type="hidden" name="subedit" value="1" />
+			<input type="submit" value="Edit Account" />
+		</p>
+	</form>
+</div>
+<?php
 
 }
 }
 
 ?>
-	</body>
+</div>
+</body>
 </html>
 <?php
 	include("footer.php");
--- userinfo.php
+++ userinfo.php
@@ -1,9 +1,24 @@
-<?php
-include("top_header.php");
-include("header.php");
-$page = "userinfo.php";
-?>
+<html>
 
+<head>
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <link rel="stylesheet" type="text/css" href="userinfo.css">
+  <link rel="stylesheet" type="text/css" href="navbar.css">
+</head>
+
+<body>
+
+<div class="card card-2">
+  <h5><strong>Logged In.</strong></h5>
+    Welcome <strong>admin</strong>, you are logged in.
+</div>
 <div class="row">
         <div class="col s12 m6">
           <div class="card">
@@ -32,69 +47,10 @@ $page = "userinfo.php";
 
               })
           </script>
-
-
-<div>
-<?php
-/* Requested Username error checking */
-$req_user = trim($_GET['user']);
-if(!$req_user || strlen($req_user) == 0 ||
-   !preg_match("/^([0-9a-z])+$/i", $req_user) ||
-   !$database->usernameTaken($req_user)){
-   die("Username not registered");
-}
-
-if(MAIL){
- $q = "SELECT mail_id FROM ".TBL_MAIL." WHERE UserTo = '$session->username' and status = 'unread'";
- $numUnreadMail = $database->query($q) or die(mysql_error());
- $numUnreadMail = mysql_num_rows($numUnreadMail);
- 
- echo "<div class='grid_5'><p class='right'>[<a href=\"mail.php\">You have $numUnreadMail Unread Mail</a>]&nbsp;</p></div>";
- echo "<div class='clear'></div>";
-}
-
-/* Logged in user viewing own account */
-if(strcmp($session->username,$req_user) == 0){
-   echo "<h1>My Account</h1>";
-}
-/* Visitor not viewing own account */
-else{
-   echo "<h1>User Info</h1>";
-}
-
-/* Display requested user information */
-$req_user_info = $database->getUserInfo($req_user);
-
-/* Name */
-echo "<p><b>Name: ".$req_user_info['name']."</b><br />";
-
-/* Username */
-echo "<p><b>Username: ".$req_user_info['username']."</b><br />";
-
-/* Email */
-echo "<b>Email:</b> ".$req_user_info['email']."</p>";
-
-/**
- * Note: when you add your own fields to the users table
- * to hold more information, like homepage, location, etc.
- * they can be easily accessed by the user info array.
- *
- * $session->user_info['location']; (for logged in users)
- *
- * ..and for this page,
- *
- * $req_user_info['location']; (for any user)
- */
-
-/* If logged in user viewing own account, give link to edit */
-if(strcmp($session->username,$req_user) == 0){
-   echo "<a href=\"useredit.php\">Edit Account Information</a><br><br>";
-}
-?>
-</div>
-<?php
-include("footer.php");
-?>
-
 </body>
+
 </html>
+
+<?php
+include('footer.php');
+?>
